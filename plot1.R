f4e5ec53cdae1dabcef331408cf77208bf14d7c1
Caton Beitzell <caton@beitzell.com>
2015-10-09 02:07:35 -0400

    more changes

--- plot1.R
+++ plot1.R
@@ -1,13 +1,31 @@
+# The read_power_data script contains the code to get, read int, and
+# manipulate data found in the household_power_consumption.txt
+# file.  The resulting data will be a data.table called energyDT.
 source("read_power_data.R")
 
 library(data.table)
 
 plot1 <- function() {
 
+        # check to see if the energyDT data.table exists. If
+        # it doesn't, then call the read_power_data function
+        # to create it.  The read_power_data function resides
+        # in the read_power_data.R script.
         if(!(exists("energyDT"))) read_power_data()
+        
+        # set the plotting device to be a png.  It is height and 
+        # width of 480.
         png(filename = "plot1.png",width=480,height=480,type = "windows")
+        
+        # In case the rows for plotting have changed, set it
+        # so one graph will be drawn.
         par(mfrow = c(1,1))
+        
+        # Using the energyDT data, create a histogram for the Global 
+        # Active Power variable.
         with(energyDT,hist(Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)"))
+        
+        # Close out the plotting device.
         dev.off()        
 }
 
--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ library(data.table)
 plot4 <- function() {
         
         if(!(exists("energyDT"))) read_power_data()
-        png(filename = "plot4.png",width=480,height=480,type = "windows")
+        png(filename = "plot4.png",bg = "transparent",width=480,height=480,units = "px",pointsize = 12, type = "cairo")
         par(mfrow = c(2,2))
         with(energyDT,{
                 plot(datetime,Global_active_power, type = "l",xlab = "",ylab = "Global Active Power")
@@ -14,7 +14,8 @@ plot4 <- function() {
                 points(datetime,Sub_metering_2,col = "red", type = "l")
                 points(datetime,Sub_metering_3,col = "blue",type = "l")
                 legend("topright",lwd = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty = "n")
-                plot(datetime,Global_reactive_power, type = "l",xlab = "datetime")
+                plot(datetime,Global_reactive_power, type = "n",xlab = "datetime")
+                lines(datetime,Global_reactive_power)
         })
         dev.off()
 }
